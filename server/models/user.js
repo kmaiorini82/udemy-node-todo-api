7061a80d36dec54c1dd5aee0a885e920e9a04fc9
KJM <kmaiorini@gmail.com>
2019-01-30 14:30:46 -0500

    Added POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,95 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+// Schema allows us to add custom functions
+var UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         minLength: 1,
-        trim: true
-    }
-})
+        trim: true,
+        unique: true,
+        validate: {
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minLength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+// Define instance methods
+// Not using arrow function because it doesn't 
+// bind this
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth'
+    var token = jwt.sign({_id: user._id.toHexString(), access},'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    // Returning token allows promise to be chained
+    // Then clause of who calls this will receive 
+    // token in their success call.
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+UserSchema.methods.toJSON = function () {
+    return _.pick(this, ['_id', 'email']);
+};
+
+var User = mongoose.model('User', UserSchema);
+
+// Create model this way for simple models
+// var User = mongoose.model('User', {
+//     email: {
+//         type: String,
+//         required: true,
+//         minLength: 1,
+//         trim: true,
+//         unique: true,
+//         validate: {
+//             validator: (value) => {
+//                 return validator.isEmail(value);
+//             },
+//             message: '{VALUE} is not a valid email'
+//         }
+//     },
+//     password: {
+//         type: String,
+//         require: true,
+//         minLength: 6
+//     },
+//     tokens: [{
+//         access: {
+//             type: String,
+//             required: true
+//         },
+//         token: {
+//             type: String,
+//             required: true
+//         }
+//     }]
+// })
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+// TODOS
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -27,7 +28,7 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, (err) => {
+    }).catch((err) => {
         res.status(400).send(err);
     });
 });
@@ -35,7 +36,7 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
-    }, (err) => {
+    }).catch((err) => {
         res.status(400).send(err);
     });
     
@@ -98,6 +99,22 @@ app.patch('/todos/:id', (req, res) => {
         res.status(500).send(err);
     });
 
+});
+
+// USERS
+
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
 });
 
 app.listen(port, () => {
