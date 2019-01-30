9c5cd5970f33e8f8c7b9e3cfbf1e60b494f5831e
KJM <kmaiorini@gmail.com>
2019-01-30 17:11:26 -0500

    Added user password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
-
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
 
-var token = jwt.sign(data, '123abc');
-console.log('Token:', token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc')
-console.log('Decoded:', decoded);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log('Hash:', hash);
+    }); 
+})
+
+var hashedPassword = '$2a$10$Q5nG4hAYFlUyryiEgc65Legnft.QjllHIVDHyWpJY/YCkm0khvd3G';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log('Res:', res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log('Token:', token);
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log('Decoded:', decoded);
 
 // var message = 'I am user number 3'
 // var hash = SHA256(message).toString();
